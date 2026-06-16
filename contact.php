4cdf272d10e8b53c11f49b447611ec9950258425
John leo <ummercool559@gmail.com>
2026-06-16 22:05:54 +0800

    style: added iframes for map view location

--- contact.php
+++ contact.php
@@ -41,6 +41,32 @@
   </div>
  </section>
 
+ <!-- address section -->
+ <section>
+  <div class="location-wrapper">
+   <h2>Address:</h2>
+   <div class="map-wrapper">
+    <iframe class="map-view-location" src="https://www.google.com/maps/embed?pb=!4v1781616262044!6m8!1m7!1sY-La8SPdonQqOjQRfTHVsw!2m2!1d14.22136556369745!2d121.7463186542754!3f275.5749409757922!4f3.8129971193873473!5f0.4000000000000002" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe class="map-view-location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7735.070502226265!2d121.74276888370518!3d14.22196932864901!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x339806ef25555327%3A0x358bd72389452ca0!2sCagsiay%20I%20National%20High%20School!5e0!3m2!1sen!2sus!4v1781617050258!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   </div>
+   <div class="address-wrapper">
+    <div class="details">
+     <label>SCHOOL ADDRESS:</label>
+     <p>Sitio Kamagong, Brgy. Cagsiay I, Mauban, Quezon</p>
+    </div>
+    <div class="details">
+     <label>CONTACT NUMBER:</label>
+     <p>(042) 373 6952 / 0947 152 3064</p>
+    </div>
+    <div class="details">
+     <label>EMAIL ADDRESS:</label>
+     <a href="mailto:308005@deped.gov.ph">308005@deped.gov.ph</a>
+    </div>
+   </div>
+  </div>
+ </section>
+
+
  <script src="public/js/header.js"></script>
 </body>
 
