a717a03ba51935e0e2b6efeb8caaa8661bb3e6c5
John leo <ummercool559@gmail.com>
2026-06-16 20:34:04 +0800

    style: added contact page and school head section

---
+++ contact.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <meta name="author" content="John Leo Jariel">
+ <meta name="description" content="Accessible website for students who wants to find research references.">
+ <link rel="stylesheet" href="public/css/contact.css">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/dist/tabler-icons.min.css" />
+ <title>C1NHS - Research Repository</title>
+</head>
+
+<body>
+ <!-- header -->
+ <?php include './includes/header.php' ?>
+
+ <section>
+  <div class="contacts-header">
+   <div class="subheader">
+    <h4>C1NHS</h4>
+    <p>Research Repository</p>
+   </div>
+
+   <h1>Contact</h1>
+
+   <p>Have a question about the repository, need help finding a specific study, or just want to share some feedback? We’re here to help. Drop us a message below or use our contact details to get in touch, and we’ll do our best to point you in the right direction.</p>
+  </div>
+ </section>
+
+ <section>
+  <div class="school-head-wrapper">
+   <div class="school-head-header">
+    <h2>SCHOOL</h2>
+    <h2>HEAD</h2>
+   </div>
+   <div class="img-wrapper">
+    <img src="public/img/wilmor-impreso.webp" alt="" width="934">
+   </div>
+   <h2 class="school-head-name">Wilmor E. Impreso</h2>
+  </div>
+ </section>
+
+ <script src="public/js/header.js"></script>
+</body>
+
+</html>
